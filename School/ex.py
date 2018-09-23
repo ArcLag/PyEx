31dabee26c571bdf21a5e8af231727d83ce5722d
Wiebesu <caglar_G@live.nl>
2018-09-23 13:39:57 +0200

    ex

---
+++ School/ex.py
@@ -0,0 +1,21 @@
+print("\n""##########")
+print("#" + "\t" + "\t" + " #")
+print("#" + "\t" + "\t" + " #")
+print("#" + "\t" + "\t" + " #")
+print("##########" + "\n")
+
+for i in range(0, 10):
+    print("#", end='')
+
+print("\t")
+
+for i in range(0, 3):
+    print("#" + " " + " " + " " + " " + " " + " " + " " + " " + "#")
+
+for i in range(0, 10):
+    print("#", end='')
+
+print("\n")
+
+
+print("##########" +"\n" + "# \t \t #" + "\n" + "# \t \t #" + "\n"+"# \t \t #" + "\n##########")
