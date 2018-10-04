11be668bd3fa5018675a374d2c35eeb9031ec901
Wiebesu <caglar_G@live.nl>
2018-10-05 00:06:01 +0200

    Exercises

---
+++ Analysis/ex4.py
@@ -0,0 +1,6 @@
+A = {'a', 'b', 'c'}
+B = {1, 2, 3}
+
+for A in {'a', 'b', 'c'}:
+    for B in {1, 2, 3}:
+        print((str(A)+ ',' + str(B)))
---
+++ Dev/Test.py
@@ -0,0 +1,8 @@
+n = 9
+
+if n%3==0:
+    s = str(n)+" is divisble by 3"
+else:
+    s = str(n)+" is NOT divisble by 3"
+
+print(s)
